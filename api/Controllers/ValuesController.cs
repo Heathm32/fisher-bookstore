a84c71951e22fba40b33846b846da573d43a964f
Heath Monroe <monroe.1150@buckeyemail.osu.edu>
2019-02-13 18:58:51 -0500

    database built

--- api/Controllers/ValuesController.cs
+++ api/Controllers/ValuesController.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
 namespace Fisher.Bookstore.Api.Controllers
 {
     [Route("api/[controller]")]
---
+++ api/Models/Book.cs
@@ -0,0 +1,8 @@
+using System;
+
+namespace Fisher.Bookstore.Models{
+
+    public class Book{
+
+    }
+}
